40a8ddf7ac4c19f58246b745f2990ead5e8aa745
Evgymart <alignant@yandex.ru>
2024-04-03 18:32:36 +0300

    Add first migration

---
+++ migrations-db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'dbname' => 'main',
+    'user' => 'main',
+    'password' => 'main',
+    'host' => 'database',
+    'driver' => 'pdo_pgsql'
+];
---
+++ migrations/Version20240403150751.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240403150751 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Creates a table for temperature reading';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $sql = <<<SQL
+        CREATE TABLE temperature_reading (
+            id INT GENERATED ALWAYS AS IDENTITY,
+            sensor_uuid VARCHAR(255) NOT NULL,
+            reading_time TIMESTAMP NOT NULL
+        );
+SQL;
+
+        $this->addSql($sql);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("DROP TABLE temperature_reading;");
+    }
+}
